971b0cf38ef6fb04c4a4e3ef9f7ab6e2a8fdfe98
99kr <xloziiehd@gmail.com>
2022-02-15 04:45:55 +0100

    mobile nav

--- src/components/01 header-v1/header-popup.jsx
+++ src/components/01 header-v1/header-popup.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import './header-popup.css';
-import MenuIcon from './../../icons/lordicon/menu-icon.gif'
 
-function PopupHeader() {
+function PopupHeader({ popup, hidePopup }) {
   return (
-    <div className="popup-header">
-        <div className="container">
-            <ul>
-                <li><a data-scroll-nav="0" href="#" className="active2">Home</a></li>
-                <li><a data-scroll-nav="1" href="#">What we do</a></li>
-                <li><a data-scroll-nav="2" href="#">Cases</a></li>
-                <li><a data-scroll-nav="3" href="#">Testimonials</a></li>
-                <li><a data-scroll-nav="4" href="#">Our Story</a></li>
-                <li><a data-scroll-nav="5" href="#">Team</a></li>
-                <li><a data-scroll-nav="6" href="#">Try us</a></li>
-            </ul>
-        </div>
+    <div className={`popup-header ${popup ? "active" : ""}`}>
+      <div className="backdrop" onClick={hidePopup} />
+      <div className="container">
+        <ul>
+          <li><a onClick={hidePopup} data-scroll-nav="0" href="#" className="active2">Home</a></li>
+          <li><a onClick={hidePopup} data-scroll-nav="1" href="#">What we do</a></li>
+          <li><a onClick={hidePopup} data-scroll-nav="2" href="#">Cases</a></li>
+          <li><a onClick={hidePopup} data-scroll-nav="3" href="#">Testimonials</a></li>
+          <li><a onClick={hidePopup} data-scroll-nav="4" href="#">Our Story</a></li>
+          <li><a onClick={hidePopup} data-scroll-nav="5" href="#">Team</a></li>
+          <li><a onClick={hidePopup} data-scroll-nav="6" href="#">Try us</a></li>
+        </ul>
+      </div>
     </div>
   );
 }
--- src/components/01 header-v1/header.jsx
+++ src/components/01 header-v1/header.jsx
@@ -1,32 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './sticky-header.css';
 import MenuIcon from './../../icons/lordicon/menu-icon.gif'
 import LogoIcon from './../../images/LOGO-C1.png'
+import PopupHeader from './header-popup';
 
 function HeaderV1() {
+  const [popup, setPopup] = useState(false);
+
+  useEffect(() => {
+    updateScreenWidth();
+
+    window.addEventListener("resize", updateScreenWidth);
+
+    return () => window.removeEventListener("resize", updateScreenWidth);
+  }, []);
+
+  const updateScreenWidth = () => {
+    if (window.innerWidth > 850) {
+      setPopup(false);
+    }
+  }
+
+  const togglePopup = () => {
+    setPopup(!popup);
+  }
+
+  const hidePopup = (e) => {
+    setPopup(false);
+  }
+
   return (
-    <div className="header">
-      <div className="header-inner-container">
-        <div className="header-logo">
-          <img src={LogoIcon}></img>
-          <h1>ESPORTS TALENT AGENCY</h1>
-        </div>
-        <div className="header-nav-bar">
-          <ul>
-            <li><a data-scroll-nav="0" href="#" className="active2">Home</a></li>
-            <li><a data-scroll-nav="1" href="#">What we do</a></li>
-            <li><a data-scroll-nav="2" href="#">Cases</a></li>
-            <li><a data-scroll-nav="3" href="#">Testimonials</a></li>
-            <li><a data-scroll-nav="4" href="#">Our Story</a></li>
-            <li><a data-scroll-nav="5" href="#">Team</a></li>
-            <li><a data-scroll-nav="6" href="#">Try us</a></li>
-          </ul>
-        </div>
-        <div className="header-mobile-nav-bar">
-          <a><i class="bi bi-list"></i></a>
+    <>
+      <div className="header">
+        <div className="header-inner-container">
+          <div className="header-logo">
+            <img src={LogoIcon} />
+            <h1>ESPORTS TALENT AGENCY</h1>
+          </div>
+          <div className="header-nav-bar">
+            <ul>
+              <li><a data-scroll-nav="0" href="#" className="active2">Home</a></li>
+              <li><a data-scroll-nav="1" href="#">What we do</a></li>
+              <li><a data-scroll-nav="2" href="#">Cases</a></li>
+              <li><a data-scroll-nav="3" href="#">Testimonials</a></li>
+              <li><a data-scroll-nav="4" href="#">Our Story</a></li>
+              <li><a data-scroll-nav="5" href="#">Team</a></li>
+              <li><a data-scroll-nav="6" href="#">Try us</a></li>
+            </ul>
+          </div>
+          <div className="header-mobile-nav-bar">
+            <a onClick={togglePopup}><i class="bi bi-list"></i></a>
+          </div>
         </div>
       </div>
-    </div>
+
+      <PopupHeader hidePopup={hidePopup} popup={popup} />
+    </>
   );
 }
 
