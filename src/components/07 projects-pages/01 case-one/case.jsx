7d20ca839c7ea52d5e072520ee6b372a521859bf
99kr <xloziiehd@gmail.com>
2022-02-15 21:33:20 +0100

    mobile nav - transition on out

--- src/components/07 projects-pages/01 case-one/case.jsx
+++ src/components/07 projects-pages/01 case-one/case.jsx
@@ -3,21 +3,21 @@ import HeaderV2 from '../../01 header-v2/header';
 import Intro from './01 intro-section/intro-section';
 import About from './02 about-section/about-section';
 import Testimonial from './03 testimonial-section/testimonial-section';
-import Gallery from './03 gallery-section/gallery-section';
+// import Gallery from './03 gallery-section/gallery-section';
 import Footer from '../../02 footer/footer';
 import './case.css';
 
 function caseOne() {
-    return (
-      <div className="case-page">
-        <HeaderV2 />
-        <Intro />
-        <About />
-        <Testimonial />
-        <Gallery />
-        <Footer />
-      </div>
-    );
-  }
-  
-  export default caseOne
+  return (
+    <div className="case-page">
+      <HeaderV2 />
+      <Intro />
+      <About />
+      <Testimonial />
+      {/* <Gallery /> */}
+      <Footer />
+    </div>
+  );
+}
+
+export default caseOne
