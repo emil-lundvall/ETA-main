d701ea58052028ee59ac809c500d9b50decdb04a
Anton Persson <antonsomoza@hotmail.com>
2022-02-18 17:46:27 +0100

    .
    .

--- src/components/05 esport-info-page/esport-intro/esport-intro.jsx
+++ src/components/05 esport-info-page/esport-intro/esport-intro.jsx
@@ -10,21 +10,35 @@ function esportIntro() {
     <div className="esport-intro">
         <div className="intro-section" data-scroll-index="11">
             <div className="intro-inner-section" >
-                <h1>LEARN WHAT ESPORTS IS<b>.</b></h1>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <h1>FIRST, WHAT IS ESPORTS<b>?</b></h1>
+                <p>Esports (or electronic sports) is a term used to describe competitive video gaming.<br></br><br></br>
+The difference from standard video gaming is that esports is competitive (human-vs-
+human) and usually has an engaging spectator element to it, like traditional sports, where skill
+and professionalism is celebrated.<br></br><br></br>
+Esports is also unique in that a player’s success is independent of gender, how tall,
+strong, thin, or fast they are. Though there is certainly valuable insight linking good physical
+health to enhanced gamer performance, it is ultimately inconsequential whether the gamer
+stands at 5’2 or 6’8 tall.</p>
             </div>
         </div>
 
-        <div className="info-esports-section" data-scroll-index="12">
+        <div className="info-esports-section ies-color" data-scroll-index="12">
             <div className="info-esports-inner-section info-reverse">
                 <div className="esports-left-text">
                     <div className="esports-title">
-                        <h1>What is E-Sports<b>?</b></h1>
+                        <h1>Isn’t it just for the nerds<b>?</b></h1>
                     </div>
                     <div className="esports-text-two">
                         <div className="et-row-two">
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            <p>No - in fact, over 380 million people watch esports worldwide both online and in
+person. More people watched the 2016 world finals of popular esports game League of
+Legends (43 million viewers) than the NBA Finals Game 7 that year (31 million viewers).<br></br><br></br>
+Esports is a booming global industry where skilled video gamers play competitively.
+In the same way that traditional sports have competitions in baseball, basketball, and football,
+esports encompasses competitions across a variety of video games.<br></br><br></br>
+Contrary to common perception, esports is not simply a phenomenon occurring in the
+basements of unemployed twentysomethings; the industry is real, growing globally, and
+offers opportunities to connect and engage with passionate audiences across the globe.</p>
                         </div>
                     </div>
                 </div>
@@ -34,7 +48,7 @@ function esportIntro() {
             </div>
         </div>
 
-        <div className="info-esports-section ies-color" data-scroll-index="13">
+        <div className="info-esports-section" data-scroll-index="13">
             <div className="info-esports-inner-section-one">
                 <div className="esports-title">
                     <h1>What is E-Sports<b>?</b></h1>
@@ -54,7 +68,7 @@ function esportIntro() {
             </div>
         </div>
 
-        <div className="info-esports-section" data-scroll-index="14">
+        <div className="info-esports-section ies-color" data-scroll-index="14">
             <div className="info-esports-inner-section">
                 <div className="esports-left-img">
                     <div className="esports-image-two"></div>
@@ -67,6 +81,24 @@ function esportIntro() {
                         <div className="et-row-two">
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
                             <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div className="info-esports-section" data-scroll-index="15">
+            <div className="info-esports-inner-section info-reverse">
+                <div className="esports-center-text">
+                    <div className="esports-title-center">
+                        <h1>So you want to become a gamer<b>?</b></h1>
+                    </div>
+                    <div className="esports-center">
+                        <div className="et-row-two">
+                            <p>&quot;eSports has no barrier to entry. If you&#39;re a gamer, you&#39;re a gamer. If you want to step up your
+game and play in a competition you can do so. If you&#39;re really great and you practice a lot
+you might be on a big stage winning a million dollars. And that&#39;s open to everyone in the
+large part. It&#39;s about making opportunities for anyone to do anything.&quot;<br></br><br></br>James Dean, CEO of ESL UK</p>
                         </div>
                     </div>
                 </div>
