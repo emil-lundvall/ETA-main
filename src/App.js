057cb805050b651f2cac5ad5c0a696a16a736c23
99kr <xloziiehd@gmail.com>
2022-02-19 02:18:20 +0100

    gallery

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ import Talent from './components/06 talent-page/talent-page';
 import Esport from './components/05 esport-info-page/esport-info-text';
 import Loading from './components/03 loader/loading';
 import CaseOne from './components/07 projects-pages/01 case-one/case';
-import CaseTwo from './components/07 projects-pages/02 case-two/case';
-import CaseThree from './components/07 projects-pages/03 case-three/case';
-import CaseFour from './components/07 projects-pages/04 case-four/case';
-import CaseFive from './components/07 projects-pages/05 case-five/case';
-import CaseSix from './components/07 projects-pages/06 case-six/case';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
 function App() {
@@ -33,11 +28,6 @@ function App() {
             <Route path="/talents" element={<Talent />} />
             <Route path="/esports" element={<Esport />} />
             <Route path="/projects/case-one" element={<CaseOne />} />
-            <Route path="/projects/case-two" element={<CaseTwo />} />
-            <Route path="/projects/case-three" element={<CaseThree />} />
-            <Route path="/projects/case-four" element={<CaseFour />} />
-            <Route path="/projects/case-five" element={<CaseFive />} />
-            <Route path="/projects/case-six" element={<CaseSix />} />
         </Switch>
       </div>
     </Router>
--- src/components/07 projects-pages/01 case-one/04 gallery-section/gallery-section.jsx
+++ src/components/07 projects-pages/01 case-one/04 gallery-section/gallery-section.jsx
@@ -1,13 +1,70 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './gallery-section.css';
 import './../../../../App.css'
+import img1 from "../../../../images/case-one/1.png";
+import img2 from "../../../../images/case-one/2.jpg";
 
-function gallerySection() {
-    return (
-      <div className="default-section gallery" data-scroll-index="10">
+const images = [
+  img1,
+  img2
+]
 
-      </div>
-    );
+function GallerySection() {
+  const [currentImage, setCurrentImage] = useState(0);
+  const [timeoutId, setTimeoutId] = useState(null);
+
+  useEffect(() => {
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    const timeout = setTimeout(() => {
+      const index = currentImage === images.length - 1 ? 0 : currentImage + 1;
+      setCurrentImage(index);
+    }, 10000)
+
+    setTimeoutId(timeout);
+  }, [currentImage]);
+
+  const cycleGallery = (modifier) => {
+    let index = currentImage + modifier;
+
+    if (index < 0) {
+      index = images.length - 1
+    } else if (index >= images.length) {
+      index = 0
+    }
+
+    setCurrentImage(index);
   }
-  
-  export default gallerySection
+
+  return (
+    <div className="default-section gallery" data-scroll-index="10">
+      <div className="section-title">
+        <h1>Gallery<b>.</b></h1>
+      </div>
+      <div className="gallery-wrapper">
+        <div className="gallery-overlay">
+
+          <div className="gallery-control-container">
+            <div className="gallery-control" onClick={() => cycleGallery(-1)}>&lt;</div>
+          </div>
+
+          <div className="gallery-progress">
+            {images.map((_, idx) => (
+              <div className={currentImage === idx ? "selected" : ""} onClick={() => setCurrentImage(idx)} />
+            ))}
+          </div>
+
+          <div className="gallery-control-container">
+            <div className="gallery-control" onClick={() => cycleGallery(1)}>&gt;</div>
+          </div>
+        </div>
+
+        <img src={images[currentImage]} alt="" className="gallery-img" />
+      </div>
+    </div>
+  );
+}
+
+export default GallerySection
