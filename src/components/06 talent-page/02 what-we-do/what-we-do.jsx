bb77a3e5730eac7a6f912482762d5bc8c5362f16
Anton Persson <antonsomoza@hotmail.com>
2022-02-24 19:30:17 +0100

    .
    .

--- src/components/06 talent-page/02 what-we-do/what-we-do.jsx
+++ src/components/06 talent-page/02 what-we-do/what-we-do.jsx
@@ -2,11 +2,29 @@ import React from 'react';
 import './../../../App.css';
 import '../../../scroll-it/scrollIt.js';
 import './what-we-do.css';
+import Fade from "react-reveal/Fade";
+import Zoom from "react-reveal/Zoom";
 
 
 function whatWeDo() {
     return (
     <div className="default-section services" data-scroll-index="17">
+        <div className="default-inner-section">
+            <div className="project-text-container">
+                <Zoom>
+                    <h1>What we do<b>?</b></h1>
+                </Zoom>
+            </div>
+        </div>
+
+        <div className="default-step-row odd">
+            <div className="inner-step-end"></div>
+        </div>
+        <div className="default-step-row">
+            <div className="inner-step-start"></div>
+        </div>
+        <div className="default-step-row odd"></div>
+        <div className="default-step-row"></div>
     </div>
     )
 }
